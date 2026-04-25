d63748469b40713a126d0ec5c183dd64fef6150e
Simin Fahimi <142047455+SiminFahimi@users.noreply.github.com>
2026-04-25 10:08:41 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,18 @@
-def compute_accuracy(pred, true):
-    return (pred == true).mean()
+import numpy as np
+
+def sigmoid(z):
+    z = np.clip(z, -500, 500)
+    return 1 / (1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    a = sigmoid(z)
+    return a * (1 - a)
+
+
+def relu(z):
+    return np.maximum(0, z)
+
+
+def relu_prime(z):
+    return (z > 0).astype(float)
